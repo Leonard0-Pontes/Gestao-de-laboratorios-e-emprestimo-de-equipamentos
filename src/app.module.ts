7f507d2db966f63a859f68ef015817b0552ac0a4
Leonard0-Pontes <leoptsrn@gmail.com>
2026-07-14 20:25:05 -0300

    CRUD de usuarios 0.5

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LaboratorioModule } from './laboratorio/laboratorio.module';
 import { EquipamentoModule } from './equipamento/equipamento.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [LaboratorioModule, EquipamentoModule],
+  imports: [LaboratorioModule, EquipamentoModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
