bad29c868d8694dabf4eebd852e1b1eb82f76982
Heros Henrique <HerosHenrique439@gmail.com>
2026-07-14 22:02:21 -0300

    ajustando arquivos git

--- dist/auth/auth.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { UsuariosService, UsuarioSemSenha } from '../usuarios/usuarios.service';
-export declare class AuthService {
-    private readonly usuariosService;
-    constructor(usuariosService: UsuariosService);
-    validarUsuario(nome: string, email: string, senhaAberta: string): Promise<UsuarioSemSenha>;
-}
--- dist/auth/dto/login.dto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class LoginDto {
-    nome?: string;
-    email: string;
-    senha: string;
-}
