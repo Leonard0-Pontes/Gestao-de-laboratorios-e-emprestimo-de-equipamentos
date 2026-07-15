63e8f2cc5ba6e678b4bc07f03900c7f771aa5051
Heros Henrique <HerosHenrique439@gmail.com>
2026-07-15 00:35:01 -0300

    Atualização do docker

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
   
   app.enableCors();
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
